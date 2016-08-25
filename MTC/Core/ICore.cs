7b0cd4239409da82c470c096446bd77d5fa505b1
Rodrigo Sória <gitrodrigosoria@users.noreply.github.com>
2016-08-25 01:17:49 -0300

    Update ICore.cs

--- MTC/Core/ICore.cs
+++ MTC/Core/ICore.cs
@@ -5,16 +5,12 @@ namespace MTC.Core
 {
     public interface ICore
     {
-        [OperationContract]
         string GetQuotation();
 
-        [OperationContract]
         string GetCoinValue(string resultUrl);
 
-        [OperationContract]
         string GetCoin(string resultUrl);
 
-        [OperationContract]
         string ConvertTo(string coin);
     }
 }
