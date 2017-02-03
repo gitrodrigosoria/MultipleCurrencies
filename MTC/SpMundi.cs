be38aa46d7dc7ee80f6fd81f018b84b679fb23d6
Rodrigo Sória <gitrodrigosoria@users.noreply.github.com>
2017-02-03 16:41:03 -0200

    Update SpMundi.cs

--- MTC/SpMundi.cs
+++ MTC/SpMundi.cs
@@ -1,6 +1,7 @@
 using MTC.Core;
 using MTC.Entities;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,21 +18,23 @@ namespace MTC
             var resultUrl = WebRequestCore.GetUrlContext(URL);
 
             if (string.IsNullOrEmpty(resultUrl))
-                return string.Empty;            
-            
+                return string.Empty;
+
             var coin = GetCoin(resultUrl).Split('|');
             var coinValue = GetCoinValue(resultUrl).Split('|');
             var lstCoin = new List<Coin>();
 
             for (var index = 0; index < coin.Length; index++)
             {
-                lstCoin.Add(new Coin(){
+                lstCoin.Add(new Coin()
+                {
                     Key = ConvertTo(coin[index]),
-                    Value = coinValue[index]} 
+                    Value = coinValue[index]
+                }
                 );
             }
 
-            return JsonConvert.SerializeObject(lstCoin.OrderBy(x => x.Key));            
+            return JsonConvert.SerializeObject(lstCoin.OrderBy(x => x.Key));
         }
 
         public string GetCoinValue(string resultUrl)
@@ -40,15 +43,13 @@ namespace MTC
             var sbConValue = new StringBuilder();
             var result = string.Empty;
 
-            foreach (var item in RegexCore.GetMatches(@"<div class=""price "">[\d\s\D]+?<\/div>", resultUrl))
+            foreach (var item in RegexCore.GetMatches(@"<div class=""price"">(.*?)</div>", resultUrl))
             {
-                result = item.ToString().Replace(@"<div class=""price "">", string.Empty);
-                result = result.Replace("R$", string.Empty);
-                result = result.Replace(@"<button class=""buy pull-right"">Comprar</button>", string.Empty);
+                result = item.ToString().Replace(@"<div class=""price"">", string.Empty);
                 result = result.Replace("</div>", string.Empty);
                 sbConValue.AppendFormat("{0}|", result.Trim());
             }
-
+            
             return sbConValue.ToString().Trim().TrimEnd('|');
         }
 
@@ -58,14 +59,16 @@ namespace MTC
             var sbCoin = new StringBuilder();
             var result = string.Empty;
 
-            foreach (var item in RegexCore.GetMatches(@"class=""product-box"">[\d\s\D]+?<img", resultUrl))
+            foreach (var item in RegexCore.GetMatches(@"<img src=""image/flags[\d\s\D]+?<div class=""price", resultUrl))
             {
-                result = item.ToString().Replace(@"<div class=""title small"">", string.Empty);
-                result = result.Replace("<img", string.Empty);
-                result = result.Split('-')[2];
+                result = item.ToString()
+                    .Replace("flag-icon", "flagicon")
+                    .Split('-')[1]
+                    .Substring(0, 4);
+
                 sbCoin.AppendFormat("{0}|", result.Trim());
             }
-
+            
             return sbCoin.ToString().Trim().TrimEnd('|');
         }
 
